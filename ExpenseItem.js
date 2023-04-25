a39280daa29654ced97e6124fd1cfa82a0db8a61
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-04-25 22:37:35 +0530

    Addition in expense item task

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,10 +1,33 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
     return  (
-        <div>
-            <div>Food Rs 10</div>
-            <div>Petrol Rs 100</div>
-            <div>Movies Rs 200</div>
-        </div>
+            <div className='expense-item'>
+                <div className='expense-item__description'>
+                    <h2>Car Insurance</h2>
+                </div>
+                <div className='expense-item__price'>
+                    Rs 300
+                </div>
+                <div className='expense-item__description'>
+                    <h2>Movies</h2>
+                </div>
+                <div className='expense-item__price'>
+                    Rs 200
+                </div>
+                <div className='expense-item__description'>
+                    <h2>Food</h2>
+                </div>
+                <div className='expense-item__price'>
+                    Rs 10
+                </div>
+                <div className='expense-item__description'>
+                    <h2>Petrol</h2>
+                </div>
+                <div className='expense-item__price'>
+                    Rs 100
+                </div>
+
+            </div>   
     );
 }
 
