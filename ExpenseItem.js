9f5a7ba5056714ca9948f89fe0a3e0e33b75d67d
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-05-03 20:22:19 +0530

    Task 1- Static button, Adding Event Listeners

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -3,15 +3,19 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetials'
 import './ExpenseItem.css';
 function ExpenseItem(props){
-
-
+    const clickHandler = () => {
+       console.log('CLicked');
+    }
 
     return  (
             <div className='expense-item'>
              <ExpenseDate date={props.date}></ExpenseDate>
-             <ExpenseDetails price={props.price} location={props.location}   title={props.title}/>
-                
-
+             <div className='expense-item__description'>
+                 <div className='expense-item__price'>
+            <ExpenseDetails price={props.price} location={props.location} title={props.title}/>    
+                </div>                   
+             </div>
+             <button onClick={clickHandler}>Change Title</button>                 
             </div>   
     );
 }
