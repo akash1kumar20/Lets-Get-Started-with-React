3996d1a3df82649dc19f33dc84895eb786dcd146
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-05-04 21:14:56 +0530

    Change Title and Expense - onCLick button

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,21 +1,33 @@
-
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetials'
 import './ExpenseItem.css';
 function ExpenseItem(props){
+    const [title, setTitle] = useState(props.title);
+    const [price, setPrice] = useState(props.price);
     const clickHandler = () => {
-       console.log('CLicked');
+       setTitle('New Title');
+    }
+    const deleteExpense = () => {
+        console.log('Element Deleted');
+    }
+    const changeExpense = () => {
+        setPrice(100);
     }
 
     return  (
             <div className='expense-item'>
              <ExpenseDate date={props.date}></ExpenseDate>
              <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <ExpenseDetails location={props.location}></ExpenseDetails>
                  <div className='expense-item__price'>
-            <ExpenseDetails price={props.price} location={props.location} title={props.title}/>    
+                  {price}    
                 </div>                   
              </div>
-             <button onClick={clickHandler}>Change Title</button>                 
+             <button onClick={clickHandler}>Change Title</button>
+             <button onClick={deleteExpense}>Delete</button>
+             <button onClick={changeExpense}>Change Expense</button>                      
             </div>   
     );
 }
